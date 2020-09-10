53b0836eacf91281014e8d2ed0c0c6ab830a251f
Harry <travershh@gmail.com>
2020-09-10 21:17:21 +1000

    Began contructing test suite

---
+++ src/controller/input/parseInput.js
@@ -0,0 +1,45 @@
+import fs from 'fs'
+
+const inputParser = (() => {
+    var filepath
+
+    const readInput = (args) => {
+        const input = args;
+        if (checkValidInputFormat(input)) {
+            filepath = extractFilePath(input);
+        } else {
+            throw 'Invalid input format - please refer to docs';
+        }
+        return filepath;
+    }
+
+    const readDefaultInput = () => {
+        const filepath = 'src/input.txt';
+        return filepath;
+    }
+
+    const checkValidInputFormat = (input) => {
+        return input.length == 3;
+    }
+
+    const extractFilePath = (input) => {
+        return input[2];
+    }
+
+    const checkFileExists = (filepath) => {
+        return fs.existsSync(filepath);
+    }
+
+    const getInputFileContents = (filepath) => {
+        if (checkFileExists(filepath)) {
+            const fileContents = fs.readFileSync(filepath, 'utf8');
+            return fileContents;
+        } else {
+            throw 'Filepath not found! Please check your input path';
+        }
+    }
+
+    return { readInput, readDefaultInput, getInputFileContents };
+})();
+
+export { inputParser };
---
+++ test/controller/validation/validateCommandString.test.js
@@ -0,0 +1,17 @@
+import { commandStringValidator, myMod } from '../../../src/controller/validation/validateCommandString';
+
+test('direction has an extra character', () => {
+    expect(commandStringValidator.checkCommand("PLACE 1,1,NORTHH")).toBe(false);
+  });
+
+test('empty string', () => {
+    expect(commandStringValidator.checkCommand("")).toBe(false);
+});
+
+test('lowercase letter(s) in command', () => {
+    expect(commandStringValidator.checkCommand("move")).toBe(false);
+});
+
+test('numbers with multiple digits', () => {
+    expect(commandStringValidator.checkCommand("PLACE 11,1094,WEST")).toBe(true);
+});
